992fa55e2e062d34a78d9c95ac76f7c91fc8d036
tylermorrisford <tylerford@gmail.com>
2020-02-26 11:40:20 -0700

    Updates

---
+++ precache-manifest.777d07ea211c73f30278916b974f4f15.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3264275d794d1025f4f389332c4c24c5",
+    "url": "/antv-demo/index.html"
+  },
+  {
+    "revision": "c0316c1f5431f2205dde",
+    "url": "/antv-demo/static/js/2.b9c8b183.chunk.js"
+  },
+  {
+    "revision": "081c25a479b8c6e56cd02b8034dc836f",
+    "url": "/antv-demo/static/js/2.b9c8b183.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8457dec68bd47e8a8990",
+    "url": "/antv-demo/static/js/main.a10c519b.chunk.js"
+  },
+  {
+    "revision": "018174515d0c8d780153",
+    "url": "/antv-demo/static/js/runtime-main.1746ab00.js"
+  }
+]);
